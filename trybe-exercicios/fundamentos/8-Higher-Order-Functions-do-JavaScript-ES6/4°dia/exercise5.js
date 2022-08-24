008cd77389af8c9e7b9dd0a71fe90db349302ac4
Anthony dos Santos Valério <adsv2811@gmail.com>
2022-08-24 17:02:52 -0300

    resolvendo exercicio 5 com .reduce dentro de .reduce

---
+++ trybe-exercicios/fundamentos/8-Higher-Order-Functions-do-JavaScript-ES6/4°dia/exercise5.js
@@ -0,0 +1,16 @@
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+function containsA() {
+  // escreva seu código aqui
+  return names.reduce((acc, currWord) => 
+  acc + currWord.split('').reduce((acumulator, currLetter) =>  {
+    if (currLetter === 'a' || currLetter === 'A') return acumulator + 1;
+    return acumulator;
+  }, 0), 0);
+}
+
+console.log(containsA());
